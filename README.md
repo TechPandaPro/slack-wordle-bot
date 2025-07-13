6ec9561ef97666312cfd57b5db6b9f19ca83bf74
TechPandaPro <javaisntcoffee@gmail.com>
2025-07-13 00:56:24 -0700

    Migrate from Signal to Slack

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # signal-wordle-bot
 
-Play wordle in Signal. Written in TypeScript and powered by [signal-cli](https://github.com/AsamK/signal-cli).
+Play Wordle in Slack. Written in TypeScript and powered by [signal-cli](https://github.com/AsamK/signal-cli).
 
-Special thanks to [TapuCosmo/signal-bot](https://github.com/TapuCosmo/signal-bot) and [How to make a Signal bot in Python](https://fabiobarbero.eu/posts/signalbot/), both of which I used as references when developing this project.
+Special thanks to [webrender/slack-wordle](https://github.com/webrender/slack-wordle) for UI/UX inspiration.
 
 Daily Wordles are retrieved directly from the NYT API.
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-import dbus from "dbus-next";
+import "dotenv/config";
+import { SocketModeClient } from "@slack/socket-mode";
+
+// const appToken = process.env.SLACK_APP_TOKEN
+
+// SocketModeClient;
+
+const slackClient = new SocketModeClient({
+  appToken: process.env.SLACK_APP_TOKEN ?? "",
+});
+
+slackClient.on("connecting", () => console.log("Connecting to Slack..."));
+
+slackClient.on("connected", () => console.log("Connected to Slack"));
+
+slackClient.on("slash_commands", async ({ body, ack }) => {
+  console.log("received!");
+  if (body.command === "/wordle") {
+    await ack({ text: "yay wordle!" });
+  }
+});
 
 (async () => {
-  const interfaces = dbus
-    .systemBus()
-    .getProxyObject("org.asamk.Signal", "/org/asamk/Signal");
-  (await interfaces)
-    .getInterface("org.asamk.Signal")
-    .then(() => console.log("done"));
+  await slackClient.start();
 })();
